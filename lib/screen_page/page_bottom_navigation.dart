06287b8721f9b20af8c0ad145ac828da6eac8d7d
Steffani Bofilwitz <steffanibofilwitzz@gmail.com>
2024-05-17 20:41:15 +0700

    Initial commit

---
+++ lib/screen_page/page_bottom_navigation.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:maps/screen_page/page_akses_kamera.dart';
+import 'package:maps/screen_page/page_home.dart';
+import 'package:maps/screen_page/page_maps.dart';
+import 'package:maps/screen_page/page_mycourse.dart';
+
+
+
+class PageBottomNavigationBar extends StatefulWidget {
+  const PageBottomNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  State<PageBottomNavigationBar> createState() =>
+      _PageBottomNavigationBarState();
+}
+
+class _PageBottomNavigationBarState extends State<PageBottomNavigationBar>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    tabController = TabController(length: 4, vsync: this);
+    tabController.addListener(_handleTabSelection);
+  }
+
+  void _handleTabSelection() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          // handle tap
+        },
+        child: TabBarView(
+          controller: tabController,
+          children: [
+            PageHome(),
+            PageMyCourses(),
+            AksesKamera(),
+            MapsFlutter(), // Tambahkan MapsFlutter ke dalam daftar anak TabBarView
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: tabController.index,
+        onTap: (index) {
+          tabController.animateTo(index);
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            activeIcon: Icon(Icons.home, color: Colors.blueAccent),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books),
+            label: 'My Courses',
+            activeIcon: Icon(Icons.library_books, color: Colors.blueAccent),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt), // Menggunakan ikon kamera
+            label: 'Kamera',
+            activeIcon: Icon(Icons.camera_alt, color: Colors.blueAccent),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map), // Menggunakan ikon peta
+            label: 'Peta',
+            activeIcon: Icon(Icons.map, color: Colors.blueAccent),
+          ),
+        ],
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.black,
+      ),
+    );
+  }
+}
